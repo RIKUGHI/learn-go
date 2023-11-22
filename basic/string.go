8023bfedfcc64705eb95dadb472cff4efd5b63b4
RikuGhi <rifkhairkhairur@gmail.com>
2023-11-22 20:01:34 +0700

    first init

---
+++ basic/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Es")
+	fmt.Println("Teh")
+	fmt.Println("Manis")
+
+	fmt.Println(len("Es"))
+	fmt.Println("Teh"[0])
+	fmt.Println("Manis"[1])
+}
