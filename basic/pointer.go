9a2bf9dda4593b85d2c039f6ff5f5c69cda58485
RikuGhi <rifkhairkhairur@gmail.com>
2023-11-25 21:52:43 +0700

    update

---
+++ basic/pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	// pass by value
+	// address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
+	// address2 := address1 // copy value
+	
+	// address2.City = "Bandung"
+	// fmt.Println(address1) // tidak berubah
+	// fmt.Println(address2) // berubah menjadi Bandung
+
+	// pass by reference
+	var address1 Address = Address{"Subang", "Jawa Barat", "Indonesia"}
+	var address2 *Address = &address1 // pointer
+
+	address2.City = "Bandung"
+	fmt.Println(address1) // ikut berubah
+	fmt.Println(address2) // berubah menjadi Bandung
+}
