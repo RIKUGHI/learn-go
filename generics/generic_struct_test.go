f12b94bbc8770587d3f5b281a8210e3e211bf8fc
RikuGhi <rifkhairkhairur@gmail.com>
2023-12-02 22:11:37 +0700

    update generics

---
+++ generics/generic_struct_test.go
@@ -0,0 +1,41 @@
+package generics
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Data[T any] struct {
+	First  T
+	Second T
+}
+
+func (d *Data[_]) SayHello(name string) string {
+	return "Hello " + name
+}
+
+func (d *Data[T]) ChangeFirst(first T) T {
+	d.First = first
+	return d.First
+}
+
+func TestData(t *testing.T) {
+	data := Data[string]{
+		First:  "Bambang",
+		Second: "Cahyo",
+	}
+
+	fmt.Println(data)
+}
+
+func TestGenericMethod(t *testing.T) {
+	data := Data[string]{
+		First:  "Bambang",
+		Second: "Cahyo",
+	}
+
+	assert.Equal(t, "Budi", data.ChangeFirst("Budi"))
+	assert.Equal(t, "Hello Bambang", data.SayHello("Bambang"))
+}
